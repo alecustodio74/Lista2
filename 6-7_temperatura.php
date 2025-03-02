1c67e512d69e1f531b488e455d5b3bab9fd04a41
Alexandre Custódio <arcs1234@hotmail.com>
2025-03-02 11:07:56 -0300

    Exercício 6 e 7
    Conversor de temperaturas

--- 6-7_temperatura.php
+++ 6-7_temperatura.php
@@ -8,7 +8,7 @@
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
   </head>
   <body>
-    <h4>6 - Conversor de temperaturas</h4>
+    <h4>6 e 7 - Conversor de temperaturas</h4>
     <form method="post" action="6_result.php"> <!-- action vai para outra página -->
             
         
